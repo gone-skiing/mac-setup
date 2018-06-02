603ef3a960353d3977b25d0678a1b244650487c6
Eugene Krylov <eugene@eugenekrylov.com>
2018-06-02 13:34:17 -0400

    Updated to python, removed .bashrc.

--- update-virtual-env.sh
+++ update-virtual-env.sh
@@ -4,12 +4,12 @@ export ENV_NAME=python365_env
 
 cd $HOME
 
-pip freeze > $HOME/mac-setup/pip-requirements.txt
+pip freeze > $HOME/dev/mac-setup/pip-requirements.txt
 
 virtualenv -p /usr/local/bin/python3 $ENV_NAME
 
 source $HOME/$ENV_NAME/bin/activate
 
-pip install -r $HOME/mac-setup/pip-requirements.txt
+pip install -r $HOME/dev/mac-setup/pip-requirements.txt
 
 echo "Update .zshrc.local to new location, remove old location, reopen all shell windows."
