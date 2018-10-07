dbaff471c2d458bd0a33d508ef03cb8ea57b82c7
Eugene Krylov <eugene@eugenekrylov.com>
2018-10-07 17:11:34 -0400

    Updated python and ansible versions. Expanded virtual environment scripts.

---
+++ virtual-env-create.sh
@@ -0,0 +1,25 @@
+#@IgnoreInspection BashAddShebang
+
+set -e
+
+sudo -H pip install virtualenv
+
+sudo -H pip install --upgrade pip
+
+export ENV_NAME=python_env
+
+cd $HOME
+
+virtualenv -p /usr/local/bin/python3 $ENV_NAME
+
+source $HOME/$ENV_NAME/bin/activate
+
+pip install -r $HOME/dev/mac-setup/pip-requirements.txt
+
+which ansible
+
+ansible --version
+
+which python
+
+python --version
---
+++ virtual-env-freeze.sh
@@ -0,0 +1,9 @@
+#@IgnoreInspection BashAddShebang
+
+cd $HOME
+
+export ENV_NAME=python_env
+
+pip freeze > $HOME/dev/mac-setup/pip-requirements.txt
+
+mv $ENV_NAME ${ENV_NAME}.old
