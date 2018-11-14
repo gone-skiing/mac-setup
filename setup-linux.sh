bdae14d75b956956354a54706d5d66f7cace8d2f
Eugene Krylov <ekrylov@healthedge.com>
2018-11-14 14:09:28 -0500

    Added terminator config file.

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# mkdir -p ~/dev
+# cd ~/dev
+# git clone https://github.com/gone-skiing/mac-setup.git
+# create secrets.local.yml in ~/dev/mac-setup with
+#   git_token
+
 set -e
 
 ansible-playbook setup-linux.yml -i inventory $*
