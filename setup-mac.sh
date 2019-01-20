4d52bc00f149364526c9a9b56c62fd6cac5c5d98
Eugene Krylov <eugene@eugenekrylov.com>
2019-01-20 14:05:17 -0500

    Added freeze command to capture latest versions of pip installed modules.

--- setup-mac.sh
+++ setup-mac.sh
@@ -14,6 +14,10 @@ popd
 
 ansible-playbook setup-mac.yml -i inventory $* --tags "dev-tools"
 
+# Cleanup and validate home brew installation
 brew cleanup -s
 brew doctor
 brew missing
+
+# Freeze latest versions after pip updates
+virtual-env-freeze.sh
