37b5f1e434b0be68c8bb7bb942850db645289eed
Eugene Krylov <eugene@eugenekrylov.com>
2019-02-23 12:54:17 -0500

    Updated pip requirements. Swapped order of freeze and brew doctor runs. Added deactivate of python environment before running brew doctor to avoid warnings about config files in the virutal environment.

--- setup-mac.sh
+++ setup-mac.sh
@@ -14,10 +14,14 @@ popd
 
 ansible-playbook setup-mac.yml -i inventory $* --tags "dev-tools"
 
-# Cleanup and validate home brew installation
-brew cleanup -s
-brew doctor
-brew missing
 
 # Freeze latest versions after pip updates
 virtual-env-freeze.sh
+
+# Cleanup and validate home brew installation
+# First deactivate python environment
+deactivate
+
+brew cleanup -s
+brew doctor
+brew missing
