f1e7c7060a773c1adc81928f144b64db3a7d4cd9
Eugene Krylov <eugene@eugenekrylov.com>
2019-03-02 21:36:43 -0500

    Latest update.

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/local/bin/zsh
 
 set -e
 
@@ -14,14 +14,10 @@ popd
 
 ansible-playbook setup-mac.yml -i inventory $* --tags "dev-tools"
 
-
 # Freeze latest versions after pip updates
-virtual-env-freeze.sh
+./virtual-env-freeze.sh
 
 # Cleanup and validate home brew installation
-# First deactivate python environment
-deactivate
-
 brew cleanup -s
 brew doctor
 brew missing
