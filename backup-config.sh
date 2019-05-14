4bd72f11df566a478a5c5c3f461ec342017d868d
Eugene Krylov <eugene@eugenekrylov.com>
2019-05-14 08:46:15 -0400

    Fixed destination for backups

--- backup-config.sh
+++ backup-config.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-rsync -a --delete ~/Library/Application\ Support/Launchbar ~/Documents/Personal/Configuration/Home/Library/Application\ Support
-cp ~/Library/Preferences/at.obdev.LaunchBar.* ~/Documents/Personal/Configuration/Home/Library/Preferences
+backup_dir=~/GoogleDrive/Personal/Configuration
+
+rsync -a --delete ~/Library/Application\ Support/Launchbar $backup_dir/Home/Library/Application\ Support
+cp ~/Library/Preferences/at.obdev.LaunchBar.* $backup_dir/Home/Library/Preferences
