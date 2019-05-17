16b0b5293353ff2eec14acabce2418b3f0178298
Eugene Krylov <eugene@eugenekrylov.com>
2019-05-17 11:59:51 -0400

    Added more apps to backup preferences.

--- backup-config.sh
+++ backup-config.sh
@@ -2,5 +2,29 @@
 
 backup_dir=~/GoogleDrive/Personal/Configuration
 
-rsync -a --delete ~/Library/Application\ Support/Launchbar $backup_dir/Home/Library/Application\ Support
-cp ~/Library/Preferences/at.obdev.LaunchBar.* $backup_dir/Home/Library/Preferences
+declare -a rsync_dirs=(
+                 "Library/Application Support/Hazel"
+                 "Library/Application Support/IntelliJIdea2019.1"
+                 "Library/Application Support/iTerm2"
+                 "Library/Application Support/Launchbar"
+                 "Library/Application Support/Keyboard Maestro"
+                 "Library/Preferences/IntelliJIdea2019.1"
+                 )
+
+for dir in "${rsync_dirs[@]}"
+do
+    rsync -a --delete ~/"$dir/" $backup_dir/Home/"$dir"
+done
+
+declare -a pref_files=(
+                 at.obdev.LaunchBar.*
+                 com.jetbrains.intellij.plist
+                 jetbrains*
+                 com.googlecode.iterm2.*
+                 com.noodlesoft.Hazel.plist
+                 )
+
+for pref_file in "${pref_files[@]}"
+do
+    cp ~/Library/Preferences/$pref_file $backup_dir/Home/Library/Preferences
+done
